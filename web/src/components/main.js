10a92599f0d6815d192afedc6b5ee8a48e229c36
MHwishes <1025383143@qq.com>
2017-06-13 20:05:49 +0800

    add style for web

--- web/src/components/main.js
+++ web/src/components/main.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {render} from "react-dom";
+import '../style/index.less';
 
 class HelloMessage extends React.Component {
 
     render() {
-        return <div>Hello World</div>;
+        return <div className="yu">Hello World</div>;
     }
 }
 render(<HelloMessage />, document.getElementById('app'));
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -5,29 +5,61 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
- entry: {
-    'index': './src/main.js'
+  entry: {
+    'index': './src/components/main.js'
   },
 
-    output: {               //打包后的文件
-        path: __dirname+ '/public/assets/',   //表示使用绝对路径
-        filename: '[name].js' //输出文件名
+  output: {               //打包后的文件
+    path: __dirname + '/public/assets/',   //表示使用绝对路径
+    filename: '[name].js' //输出文件名
 
-    },
-    module: {
-        loaders: [            //用于加载一些静态文件夹（css样式，图片之类）
-          { test: /\.css$/, loader: "style!css" },
-          {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/, //不进行转换的文件，可以提高打包速度
-                query: {
-                    presets: ['react', 'es2015']
-                }
-           }
-        ]
-    },
-    plugins: [
+  },
+  module: {
+    rules: [            //用于加载一些静态文件夹（css样式，图片之类）
+      {
+        test: /\.css$/, loader: "style!css"
+      },
+      {
+        test: /\.js?$/,
+        include: [
+          path.resolve(__dirname, '/src')
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/, //不进行转换的文件，可以提高打包速度
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: "style-loader",
+          loader: "css-loader!less-loader"
+        })
+      },
+
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader?limit=10000&name=/build/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendors',
       filename: 'vendors.js'
@@ -35,7 +67,7 @@ var config = {
     new ExtractTextPlugin('[chunkhash:8].[name].css'),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      minify: {collapseWhitespace: true},
+      minify: { collapseWhitespace: true },
       template: path.join(__dirname, 'index.html'),
       inject: true,
       chunks: ['index', 'vendors']
@@ -43,7 +75,7 @@ var config = {
   ],
   resolve: {
     alias: {
-      'bootstrap.css': 'bootstrap/dist/css/bootstrap.min.css'
+      'bootstrap.css': 'bootstrap/public/css/bootstrap.min.css'
     }
   }
 };
