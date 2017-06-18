4271cb4ddb312f718f30a18f792f0ec6ea4a8a83
MHwishes <1025383143@qq.com>
2017-06-18 11:40:24 +0800

    backend //the failture mapper test

--- back_end/src/test/java/com/tw/rs/mapper/SectionMapperTest.java
+++ back_end/src/test/java/com/tw/rs/mapper/SectionMapperTest.java
@@ -1,8 +1,21 @@
 //package com.tw.rs.mapper;
+//import com.tw.rs.bean.Section;
+//import junit.framework.TestCase;
+//import org.junit.Test;
+//import com.tw.rs.mapper.TestBase;
 //
-///**
-// * Created by anda on 17-6-18.
-// */
-//public class SectionMapperTest {
+//import javax.ws.rs.core.Response;
+//import java.util.Map;
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.core.Is.is;
+//import com.tw.rs.mapper.SectionMapper;
 //
+//public class SectionMapperTest extends TestBase{
+//    private SectionMapper sectionMapper;
+//
+//    @Test
+//    public void should_return_section_by_Id_success() throws Exception {
+//       Integer id= sectionMapper.selectIdByPaperId(1);
+//       assertThat(id,is(1));
+//    }
 //}
---
+++ back_end/src/test/java/com/tw/rs/mapper/TestBase.java
@@ -0,0 +1,42 @@
+package com.tw.rs.mapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import com.tw.rs.util.DBUtil;
+import javax.ws.rs.core.Application;
+
+import com.tw.rs.mapper.UserMapper;
+import com.tw.rs.mapper.PaperMapper;
+import com.tw.rs.mapper.SectionMapper;
+import com.tw.rs.mapper.DefinitionsMapper;
+
+public class TestBase extends JerseyTest {
+    @Override
+    protected Application configure() {
+        enable(TestProperties.DUMP_ENTITY);
+
+        SqlSession session = DBUtil.getSession();
+        PaperMapper paperMapper = session.getMapper(PaperMapper.class);
+        SectionMapper sectionMapper=session.getMapper(SectionMapper.class);
+        DefinitionsMapper definitionsMapper=session.getMapper(DefinitionsMapper.class);
+        UserMapper userMapper = session.getMapper(UserMapper.class);
+
+        return new ResourceConfig().register(new AbstractBinder() {
+
+            @Override
+            protected void configure() {
+                bind(session).to(SqlSession.class);
+                bind(paperMapper).to(PaperMapper.class);
+                bind(sectionMapper).to(SectionMapper.class);
+                bind(definitionsMapper).to(DefinitionsMapper.class);
+                bind(userMapper).to(UserMapper.class);
+
+            }
+        }).packages("com.tw.rs.mapper");
+    }
+}
+
+
