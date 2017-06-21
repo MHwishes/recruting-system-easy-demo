b75b263ba3c01cac8e52e50980debaef6342b91c
MHwishes <1025383143@qq.com>
2017-06-21 16:13:57 +0800

    fix test for get all paper

--- bff/spec/unit/paper-controller-spec.js
+++ bff/spec/unit/paper-controller-spec.js
@@ -1,44 +1,22 @@
 require('should');
 const supertest = require('supertest');
-const config = require('config');
-const apiService = config.get('back_endApiService');
 const express = require('express');
 const app = require('../../app');
 const request = supertest(app);
-
-const Paper = require('../../model/paper');
+// const mockServer = require('../support/moco-server.js');
+//
+// const Paper = require('../../model/paper');
 
 describe('PaperContronller', () => {
 
-    // it('POST /items should return uri', (done) => {
-    //     const paper = {
-    //         name: 'java',
-    //         description: "java paper",
-    //         sections: [{
-    //             type: "logicPuzzle",
-    //             definition: {
-    //                 hard: 2,
-    //                 easy: 2,
-    //                 normal: 1
-    //             }
-    //         }]
-    //     };
-    //
-    //     request
-    //         .post('/paper-definitions')
-    //         .set('Content-Type', 'application/json')
-    //         .send(paper)
-    //         .expect(201)
-    //         .end(done)
-    // });
 
-    it('GET All papers',(done)=>{
+    it('GET All papers', (done) => {
         request
             .get('/paper-definitions')
             .expect(200)
             .end(done)
-    })
-
-
+    });
 
 });
+
+// http://back_end:8080/back_end/papers
