bc4ab38aebfc93a08d3246835c1de4aefe5fcb7c
MHwishes <1025383143@qq.com>
2017-06-21 16:23:36 +0800

    add tests for getdata from backend

--- bff/spec/unit/api-service-controller-spec.js
+++ bff/spec/unit/api-service-controller-spec.js
@@ -11,10 +11,53 @@ describe('api-service', () => {
         request
             .get('/papers')
             .expect(200)
-            .expect((res)=> {
-                res.body.length.should.equal(3)
-            })
             .end(done)
     });
+
+    it('POST send papers from mysql', (done) => {
+        request
+            .post('/papers')
+            .send({
+                "name": "mahong9999999999999999999",
+                "description": "4444888888",
+                "sections": [{
+                    "type": "logicPuzzle",
+                    "definitions": {
+                        "hard": 9,
+                        "easy": 9,
+                        "normal": 45
+                    }
+                }]
+            })
+            .expect(201)
+            .end(done)
+    });
+
+    it('PUT send papers from mysql', (done) => {
+        request
+            .put('/papers/2')
+            .send({
+                "id": 2,
+                "name": "test",
+                "description": "4444888888",
+                "sections": [{
+                    "type": "logicPuzzle",
+                    "definitions": {
+                        "hard": 9,
+                        "easy": 9,
+                        "normal": 0
+                    }
+                }]
+            })
+            .expect(204)
+            .end(done)
+    });
+
+    it('DELETE send papers from mysql', (done) => {
+        request
+            .delete('/papers/2')
+            .expect(204)
+            .end(done)
+    })
 
 });
